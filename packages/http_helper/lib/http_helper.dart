45f2520f7e12fb5e2153216867054b425f974bbb
bugragoksu <bugragoksu2@gmail.com>
2021-07-19 23:22:28 +0200

    added http_helper

---
+++ packages/http_helper/lib/http_helper.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+
+BaseOptions options = new BaseOptions(
+  baseUrl: "https://api.nasa.gov/mars-photos/api/v1/rovers/",
+);
+
+Dio dio = new Dio(options);
+
+class HttpHelper {
+  Future<dynamic> get(String endpoint) async {
+    try {
+      final res = await dio.get(endpoint);
+      return res.data;
+    } on DioError catch (e) {
+      throw Exception(e);
+    }
+  }
+}
--- packages/rover_repository/lib/src/rover_repository.dart
+++ packages/rover_repository/lib/src/rover_repository.dart
@@ -1,8 +1,10 @@
+import 'package:http_helper/http_helper.dart';
+
 import '../rover_repository.dart';
-import 'model/rover_model.dart';
 
 class RoverRepository {
   Future<RoverModel> getRover({required String type}) async {
-    return RoverModel(photos: []);
+    var result = HttpHelper().get('$type/photos?sol=1000&api_key=DEMO_KEY');
+    return roverFromJson(result);
   }
 }
